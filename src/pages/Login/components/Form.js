a014939239e87f37e3e9f36082175ea8cdbcbc61
Saullo <saullosap14@gmail.com>
2022-04-26 16:14:16 -0300

    Rotas da tela de login .1

--- src/pages/Login/components/Form.js
+++ src/pages/Login/components/Form.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function Form() {
   return (
     <div className="box-form">
+
       <section className="box-header"><section>LOGIN</section></section>
       
       <form className="formulario">
         <input className="login" placeholder="Digite seu email"></input>
         <input className="senha" placeholder="Digite sua senha"></input>
         <a className="recuperar-senha">Esqueceu sua senha?</a>
-        <button className="botao-entrar"><section>ENTRAR</section></button>
+        <Link to="/"><button class="login-botao entrar"><section>ENTRAR</section></button></Link>
+        <Link to="/sign-up" ><button className="login-botao cadastrar"><section>CADASTRE-SE</section></button></Link>
       </form> 
+      {/* <Link to="/">retornar a página inicial</Link> */}
 
     </div>
   );
