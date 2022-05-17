2accd7b9d36da7089588f86b224dd73d310b8a8f
fmrqs <fernandmrqs0@gmail.com>
2022-05-17 14:56:56 -0300

    Subindo lógica do login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React, { useState, useContext } from "react"
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import SignUp from "./pages/signUp"
+import {AuthProvider, AuthContext} from './services/auth'
 
 function App() {
+  const Private = ({children}) => {
+    const { authenticated, loading } = useContext(AuthContext);
+
+    if(loading)
+      return <div className="loading">Loading...</div>;
+
+    if(authenticated)
+      return <Navigate to='/home'/>
+    
+    return children
+  }
+
   return (
     <BrowserRouter>
+    <AuthProvider>
       <Routes>
-        <Route path="/home" element={<Home/>}/>
+        <Route path="/home" 
+        element={
+        <Private>
+          <Home/>
+        </Private>}/>
         <Route path="/" element={<Login/>}/>
         <Route path="/sign-up" element={<SignUp/>}/>
       </Routes>
+    </AuthProvider>
     </BrowserRouter>
   );
 }
