2b344ffe1774c47dd2e2140fc24340f71451cbc2
Saullo <saullosap14@gmail.com>
2022-06-01 16:03:27 -0300

    Juntando tudo

--- src/pages/signUp_pt1/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/pages/signUp_pt1/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./style.css";
+
+const initialValues = {
+    email:"",
+    nome:"",
+    senha:""
+}
 
 class FormularioCadastro extends Component{
     render(){
+        const onSubmit = () => {
+            
+            // post("", user)
+            // .then((response) => setUser(response.data))
+            // .catch((err) =>{ 
+            //     console.error("ops! ocorreu um erro" + err);
+            // });
+            
+        }
+
         return(
             <div className="form_cadastro">
                 <div className="header_form_title"></div>
                 <div className="header_form_logo"></div>
                 <h1 className="info_form">Informações de Usuario:</h1>
-                <form>
+                <form onSubmit={onSubmit}>
+                
+                <input 
+                type="text" 
+                placeholder="Nome"
+                className="input_nome"/>
+
+                <input 
+                type="text" 
+                placeholder="Sobrenome"
+                className="input_sobrenome"/>
+
                 <input 
                 type="text"
                 placeholder="Digite seu e-mail"
@@ -17,15 +45,10 @@ class FormularioCadastro extends Component{
                          
                 <input 
                 type="text" 
-                placeholder="Nome de Usuario"
-                className="input_usuario"/>
-
-                <input 
-                type="text" 
                 placeholder="Digite sua Senha"
                 className="input_senha"/>
 
-                <Link to={"/sign-up2"}><button id="avancar" className="botao_avancar"><section></section></button></Link>
+                <Link to={"/sign-up2"} ><button type="submit" id="avancar" className="botao_avancar"><section></section></button></Link>
                         
                 </form>
             </div>
